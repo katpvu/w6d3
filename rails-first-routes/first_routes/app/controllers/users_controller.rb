b78d4d58d355c70743521763f6824535ffec4b08
AErdenberger <erdenberger.alexander@gmail.com>
2023-03-08 17:12:10 -0800

    finish part 4

--- rails-first-routes/first_routes/app/controllers/users_controller.rb
+++ rails-first-routes/first_routes/app/controllers/users_controller.rb
@@ -1,13 +1,49 @@
+require 'byebug'
+
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        users = User.all 
+        render json: users
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity #422
+        end
     end
 
     def show
-        render json: params
+        incoming_wildcard = params[:id]
+		user = User.find(incoming_wildcard)
+		if user
+			render json: user
+		else
+			render json: ['not found'], status: 404
+		end
+    end
+
+    def update
+		incoming_wildcard = params[:id]
+		user = User.find_by(id: incoming_wildcard) # id: params[:id] also works
+		if user && user.update(user_params)
+			redirect_to user_url(user.id)
+		else
+			render json: user.errors.full_messages, status: 422 #unprocessable_entity
+		end
+	end
+
+    def destroy
+		incoming_wildcard = params[:id]
+		user = User.find(incoming_wildcard)
+		user.destroy
+        redirect_to users_url
+	end
+
+    private
+	def user_params
+		params.require(:user).permit(:name, :email)
     end
 end
